cd9e69c3e924b879728ef24d372cce0e3a8d5b21
Fraga <arthur.defragaferreira1@gmail.com>
2025-03-13 21:38:32 -0300

    reafctor: alter site

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ function mostrarMensagem() {
 
     // Condição para verificar o nome e atribuir a mensagem correspondente
     if (nome.toLowerCase() === "wesley") {
-        mensagem = "Wesley, o convite tá feito! Vai ser demais a tua presença!";
+        mensagem = "Eae Sor, como vai? Se tu tiver disposição e vontade pra comparecer nesse dia, seria incrível! Do contrário, relaxa e nem se apega!";
     } else if (nome.toLowerCase() === "theilor") {
-        mensagem = "Theilor, não posso esperar para te ver lá! Vai ser épico!";
-    } else {
-        mensagem = "Oi, " + nome + "! Você está convidado para o meu aniversário!";
+        mensagem = "Irmão, eu nem precisaria dizer o quanto quero que tu vá né. Vamos curtir o final de semana e mais ainda o domingo! Ah e não esquece da Victória, ela também está convidada.";
+    } else if (nome.toLowerCase() === "leandro") {
+        mensagem = ""
     }
 
     // Exibe a mensagem personalizada
